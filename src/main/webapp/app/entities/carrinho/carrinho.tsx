10477cff541ed63616aa9cc728b6d261901266cc
Robson Henrique <robsonhenrique16@hotmail.com>
2019-11-24 19:07:14 -0300

    [update] mostrando os produtos do carrinho conforme o ID. PS: Falta pegar o ID do carrinho do user de forma dinamica, pois tah hardcoded

--- src/main/webapp/app/entities/carrinho/carrinho.tsx
+++ src/main/webapp/app/entities/carrinho/carrinho.tsx
@@ -8,92 +8,100 @@ import { Translate, ICrudGetAllAction, getSortState, IPaginationBaseState, JhiPa
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { IRootState } from 'app/shared/reducers';
-import { getEntities } from './carrinho.reducer';
-// import { getEntity } from './carrinho.reducer';
+import { getEntity as getCarrinhoById } from './carrinho.reducer';
+import { getEntity as getClienteById } from '../cliente/cliente.reducer';
 import { getSession } from 'app/shared/reducers/authentication';
-
-// import { getUser } from 'app/modules/administration/user-management/user-management.reducer';
-
 import { ICarrinho } from 'app/shared/model/carrinho.model';
-// tslint:disable-next-line:no-unused-variable
-import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
-import { ITEMS_PER_PAGE } from 'app/shared/util/pagination.constants';
 
 export interface ICarrinhoProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
 
-export type ICarrinhoState = IPaginationBaseState;
+export type ICarrinhoState = ICarrinho;
 
 export class Carrinho extends React.Component<ICarrinhoProps, ICarrinhoState> {
-  state: ICarrinhoState = {
-    ...getSortState(this.props.location, ITEMS_PER_PAGE)
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      // id: this.props.clienteEntity.carrinho.id
+      id: 1
+    };
+  }
 
+  // setState(id:this.props.clienteEntity.carrinho.id)
   componentDidMount() {
-    this.getEntities();
+    this.props.getClienteById(this.props.account.id);
 
     this.props.getSession();
+    // this.props.getCarrinhoById(this.props.clienteEntity.carrinho.id);
+    this.props.getCarrinhoById(this.state.id);
+    // this.props.getCarrinhoById(1);
   }
-
-  sort = prop => () => {
-    this.setState(
-      {
-        order: this.state.order === 'asc' ? 'desc' : 'asc',
-        sort: prop
-      },
-      () => this.sortEntities()
-    );
-  };
-
-  sortEntities() {
-    this.getEntities();
-    this.props.history.push(`${this.props.location.pathname}?page=${this.state.activePage}&sort=${this.state.sort},${this.state.order}`);
-  }
-
-  handlePagination = activePage => this.setState({ activePage }, () => this.sortEntities());
-
-  getEntities = () => {
-    const { activePage, itemsPerPage, sort, order } = this.state;
-    this.props.getEntities(activePage - 1, itemsPerPage, `${sort},${order}`);
-  };
 
   render() {
-    const { carrinhoList, match, totalItems, account } = this.props;
+    const { match, totalItems, clienteEntity, carrinhoEntity, account } = this.props;
+
+    /* Caso o usuario do cliente ainda não esteja definido, irá "retornar", antes de redenrizar
+     * a tela
+     */
+    if (!this.props.clienteEntity.usuario) return null;
+    // if (!this.props.clienteEntity.carrinho.id) return null;
+    if (!this.props.carrinhoEntity.listItens) return null;
+    // if (!this.props.clienteEntity.carrinho.listItens) return null;
+
+    // this.setState({ id: this.props.clienteEntity.carrinho.id });
+
     return (
       <div>
         <h2 id="carrinho-heading">
           <Translate contentKey="ecommerceApp.carrinho.home.title">Carrinhos</Translate>
+          {/* ========== VALORES DE TESTE ::::::: APAGAR ::::::: ================*/}
           ## ACCOUNT ID === {account.id}
           ## ACCOUNT EMAIL === {account.email}
-          <Link to={`${match.url}/new`} className="btn btn-primary float-right jh-create-entity" id="jh-create-entity">
+          ## CLIENTE ID === {clienteEntity.id}
+          ## CLIENTE CPF === {clienteEntity.cpf}
+          {/* ## CLIENTE USER === {clienteEntity.usuario.id} */}
+          ## CLIENTE CARRINHO === {clienteEntity.carrinho.id}
+          {/* ## CLIENTE CARRINHO === {clienteEntity.carrinho.listItens[1].id} */}
+          {/* ## CLIENTE USUER ID === {this.props.clienteEntity.usuario.id} */}
+          {/* ## STATE ID DO CARRINHO === {this.state.id} */}
+          {/* ## ID DO CARRINHO === {this.props.carrinho.id} */}
+          ## STATE ID ITEM DO CARRINHO === {this.props.carrinhoEntity.listItens[0].id}
+          ## STATE PRODUTO DO CARRINHO === {this.props.carrinhoEntity.listItens[0].valorTotal}
+          {/* BOTÃO CRIAR NOVO CARRINHO */}
+          {/* <Link to={`${match.url}/new`} className="btn btn-primary float-right jh-create-entity" id="jh-create-entity">
             <FontAwesomeIcon icon="plus" />
             &nbsp;
             <Translate contentKey="ecommerceApp.carrinho.home.createLabel">Create new Carrinho</Translate>
-          </Link>
+          </Link> */}
+          {/* ===================================================================================== */}
         </h2>
         <div className="table-responsive">
-          {carrinhoList && carrinhoList.length > 0 ? (
+          {/* {carrinhoList && carrinhoList.length > 0 ? ( */}
+          {carrinhoEntity && carrinhoEntity.listItens.length > 0 ? (
             <Table responsive>
               <thead>
                 <tr>
-                  <th className="hand" onClick={this.sort('id')}>
+                  <th className="hand">
                     <Translate contentKey="global.field.id">ID</Translate> <FontAwesomeIcon icon="sort" />
                   </th>
 
-                  <th className="hand" onClick={this.sort('listItens')}>
+                  <th className="hand">
                     Produto <FontAwesomeIcon icon="sort" />
                   </th>
 
-                  <th className="hand" onClick={this.sort('listItens.quantidade')}>
+                  <th className="hand">
                     Quantidade <FontAwesomeIcon icon="sort" />
                   </th>
 
-                  <th className="hand" onClick={this.sort('totalCarrinho')}>
+                  <th className="hand">
                     <Translate contentKey="ecommerceApp.carrinho.totalCarrinho">Total Carrinho</Translate> <FontAwesomeIcon icon="sort" />
                   </th>
                 </tr>
               </thead>
+
+              {/* =================== DADOS DA TABELA DO CARRINHO ============================ */}
               <tbody>
-                {carrinhoList.map((carrinho, i) => (
+                {/* {carrinhoList.map((carrinho, i) => ( */}
+                {carrinhoEntity.listItens.map((carrinho, i) => (
                   <tr key={`entity-${i}`}>
                     <td>
                       <Button tag={Link} to={`${match.url}/${carrinho.id}`} color="link" size="sm">
@@ -102,7 +110,8 @@ export class Carrinho extends React.Component<ICarrinhoProps, ICarrinhoState> {
                     </td>
                     <td>NOME DO PRODUTO</td>
                     <td>
-                      QUANTIDADE DO PRODUTO
+                      {/* QUANTIDADE DO PRODUTO */}
+                      {carrinho.valorTotal}
                       {/* <AvField id="item-quantidade" type="text" name="quantidade" /> */}
                       {/* <AvField id="item-quantidade" type="text" name="quantidade" /> */}
                     </td>
@@ -144,35 +153,23 @@ export class Carrinho extends React.Component<ICarrinhoProps, ICarrinhoState> {
             </div>
           )}
         </div>
-        <div className={carrinhoList && carrinhoList.length > 0 ? '' : 'd-none'}>
-          <Row className="justify-content-center">
-            <JhiItemCount page={this.state.activePage} total={totalItems} itemsPerPage={this.state.itemsPerPage} i18nEnabled />
-          </Row>
-          <Row className="justify-content-center">
-            <JhiPagination
-              activePage={this.state.activePage}
-              onSelect={this.handlePagination}
-              maxButtons={5}
-              itemsPerPage={this.state.itemsPerPage}
-              totalItems={this.props.totalItems}
-            />
-          </Row>
-        </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ carrinho, authentication }: IRootState) => ({
-  carrinhoList: carrinho.entities,
+const mapStateToProps = ({ carrinho, cliente, authentication }: IRootState) => ({
+  // carrinhoList: carrinho.entities,
   totalItems: carrinho.totalItems,
-  // user: storeState.userManagement.user
-  account: authentication.account
+  clienteEntity: cliente.entity,
+  account: authentication.account,
+  carrinhoEntity: carrinho.entity
 });
 
 const mapDispatchToProps = {
   getSession,
-  getEntities
+  getCarrinhoById,
+  getClienteById
 };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
