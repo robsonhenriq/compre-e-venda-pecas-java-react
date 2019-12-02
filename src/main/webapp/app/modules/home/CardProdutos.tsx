3eeaf396436b081487bd0b31c0b661278bf04b86
Robson Henrique <robsonhenrique16@hotmail.com>
2019-12-02 00:50:20 -0300

    [add] CardProdutos Statefull

---
+++ src/main/webapp/app/modules/home/CardProdutos.tsx
@@ -0,0 +1,185 @@
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { UncontrolledCarousel, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+import { AvFeedback, AvForm, AvGroup, AvInput, AvField } from 'availity-reactstrap-validation';
+// tslint:disable-next-line:no-unused-variable
+import { Translate, translate, ICrudGetAction, ICrudGetAllAction, ICrudPutAction } from 'react-jhipster';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IRootState } from 'app/shared/reducers';
+
+import { getEntities as getMarcas } from 'app/entities/marca/marca.reducer';
+import { getEntities as getFotos } from 'app/entities/foto/foto.reducer';
+import { getEntities as getVeiculos } from 'app/entities/veiculo/veiculo.reducer';
+import { getEntities as getAvaliacaos } from 'app/entities/avaliacao/avaliacao.reducer';
+import { IVendedor } from 'app/shared/model/vendedor.model';
+import { getEntities as getVendedors } from 'app/entities/vendedor/vendedor.reducer';
+import { getEntity, updateEntity, createEntity, reset } from '../../entities/produto/produto.reducer';
+import { IProduto } from 'app/shared/model/produto.model';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+import { isListNull, isObjectNull } from 'app/shared/util/verificacoes-utils';
+import { IItem } from 'app/shared/model/item.model';
+import { createEntity as saveItem } from 'app/entities/item/item.reducer';
+import { createEntity as saveCarrinhoItem } from 'app/entities/carrinho/carrinho.reducer';
+
+export interface IProdutoUpdateProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {}
+
+// RouteComponentProps<{ id: string; title: string; marca: string; precoAVista: any }> {}
+
+export interface IProdutoUpdateState {
+  isNew: boolean;
+  idslistFotos: any[];
+  idsaplicacoes: any[];
+  idslistAvaliacao: any[];
+  marcaId: string;
+  listVendedoresId: string;
+}
+
+// CardDeProdutos para ser mostrado na tela HOME
+
+export class CardProdutos extends React.Component<IProdutoUpdateProps, IProdutoUpdateState> {
+  constructor(props) {
+    super(props);
+    // this.props.saveItem.bind(this);
+    // this.handleClick = this.handleClick.bind(this)
+  }
+
+  saveEntity = (event, errors, values) => {
+    if (errors.length === 0) {
+      const { produtoEntity } = this.props;
+      // const entity = {
+      // ...produtoEntity,
+      // ...values
+      // listFotos: mapIdList(values.listFotos),
+      // aplicacoes: mapIdList(values.aplicacoes),
+      // listAvaliacaos: mapIdList(values.listAvaliacaos)
+      // };
+
+      // const itemToSave = {
+      const itemToSave: IItem = {
+        valorTotal: produtoEntity.precoAVista,
+        valorItem: produtoEntity.precoAVista,
+        quantidade: 1,
+        produto: {
+          id: produtoEntity.id
+        }
+      };
+      let itemSalvo = {};
+      // try {
+      itemSalvo = this.props.saveItem(itemToSave);
+      // console.log('itemSalvo , ', itemSalvo);
+
+      // } catch (error) {
+      // console.log('ERRO ao tentar salvar o ITEM: ' + error);
+      // } finally {
+      //   console.log('itemSalvo do finally: ' + itemSalvo);
+      // }
+
+      // console.log('itemToSave para mandar para salvar , ', itemToSave);
+      // console.log('itemToSave para mandar para salvar + ' + itemToSave);
+    }
+  };
+
+  render() {
+    const { produtoEntity, marcas, fotos, veiculos, avaliacaos, vendedors, loading, updating } = this.props;
+    // const { isNew } = this.state;
+    // {if }
+
+    return (
+      <Fragment>
+        <AvForm className="col-4 col-md-4 col-lg-4" model={produtoEntity} onSubmit={this.saveEntity}>
+          <Card>
+            <CardImg
+              top
+              width="318px"
+              height="180px"
+              src={`data:${isListNull(this.props.produtoEntity.listFotos)[0].imagemContentType};base64,${
+                isListNull(this.props.produtoEntity.listFotos)[0].imagem
+              }`}
+              alt="Foto d produto"
+            />
+            {/* {this.props.fotos} */}
+            <CardBody>
+              <CardTitle>
+                {/* <b>Produto:</b> {this.props.title} */}
+                <b>Produto:</b> {this.props.produtoEntity.descricao}
+              </CardTitle>
+
+              {/* {props.marca && ( */}
+              {this.props.produtoEntity.marca && (
+                <CardSubtitle>
+                  {/* <b>Marca:</b> {this.props.marca} */}
+                  <b>Marca:</b> {this.props.produtoEntity.marca.nome}
+                </CardSubtitle>
+              )}
+
+              <CardText>
+                {/* <div> */}
+                <Fragment>
+                  {/* <b>Preço Á Vista R$:</b> {this.props.precoAVista} */}
+                  <b>Preço Á Vista R$:</b> {this.props.produtoEntity.precoAVista}
+                </Fragment>
+                {/* </div> */}
+
+                {/* <hr /> */}
+
+                {/* BTN adicionar produto no carrinho */}
+                {/* <div className="d-flex justify-content-center"> */}
+                <div className="d-flex justify-content-center">
+                  {/* <a className="btn btn-success btn-sm ml-12 " href="carrinho.html"> */}
+                  {/* <a tag={Link} to={`entity/carrinho`} className="btn btn-success btn-sm ml-12 "> */}
+                  {/* <Button tag={Link} to={`entity/carrinho`} className="btn btn-success btn-sm ml-12 " size="sm"> */}
+                  {/* <Button tag={Link} type="submit" to={`entity/carrinho`} className="btn btn-success btn-sm ml-12 " size="sm"> */}
+                  <Button type="submit" className="btn btn-success btn-sm ml-12 " size="sm">
+                    <span className="btn btn-success btn-sm ml-6" id="spanAdicionarProdutoCarrinho">
+                      Adicionar ao carrinho {this.props.produtoEntity.id} <FontAwesomeIcon icon={faShoppingCart} />
+                    </span>
+                    {/* </a> */}
+                  </Button>
+                </div>
+              </CardText>
+            </CardBody>
+          </Card>
+        </AvForm>
+      </Fragment>
+    );
+    // );
+  }
+}
+
+const mapStateToProps = (storeState: IRootState) => ({
+  marcas: storeState.marca.entities,
+  fotos: storeState.foto.entities,
+  veiculos: storeState.veiculo.entities,
+  avaliacaos: storeState.avaliacao.entities,
+  vendedors: storeState.vendedor.entities,
+  produtoEntity: storeState.produto.entity,
+  // itemEntity: storeState.item.entity,
+  loading: storeState.produto.loading,
+  updating: storeState.produto.updating,
+  updateSuccess: storeState.produto.updateSuccess
+});
+
+const mapDispatchToProps = {
+  getMarcas,
+  getFotos,
+  getVeiculos,
+  getAvaliacaos,
+  getVendedors,
+  getEntity,
+  updateEntity,
+  createEntity,
+  saveItem,
+  saveCarrinhoItem,
+  reset
+};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CardProdutos);
